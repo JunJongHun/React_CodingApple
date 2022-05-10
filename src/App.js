1e4c96055bc46a19b91dd9e60bc6f7fc46e2a534
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-05-10 19:59:50 +0900

    public

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import "./App.css";
-import { Button } from "react-bootstrap";
+
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{" "}
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          <Col sm>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+
+          <Col sm>
+            <img
+              src={process.env.PUBLIC_URL + "/logo192.png"}
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>sm=true</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
